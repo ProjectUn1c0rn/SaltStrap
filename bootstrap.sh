14646ba3ac0b45ea66e3fda2df0a04df4f30a9a1
ProjectUn1c0rn <project.un1c0rn@yandex.com>
2014-06-30 17:54:46 +0200

    Update bootstrap.sh
    Bootstraping trough saltstack. Masterless. 
    (Well master is this git repo ;))

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+# use "wget https://raw.githubusercontent.com/ProjectUn1c0rn/SaltStrap/master/bootstrap.sh|sudo sh"
+# salt-minion install :
+wget -O - https://bootstrap.saltstack.com | sudo sh
+cat > /etc/salt/minion <<EOF
+fileserver_backend:
+  - git
+gitfs_remotes:
+  - https://github.com/ProjectUn1c0rn/SaltStrap.git
+EOF
+salt-call --local state.highstate
