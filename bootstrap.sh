b6626ba0fa091c2a9580e96b341e17b9da3b585d
ProjectUn1c0rn <project.un1c0rn@yandex.com>
2014-07-01 11:30:35 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# as root :
-# apt-get install ca-certificates -y
-# wget -O - https://raw.githubusercontent.com/ProjectUn1c0rn/SaltStrap/master/bootstrap.sh|sh
-# salt-minion install :
+
+# Debian testing needs this :
 export BS_PIP_ALLOWED=0
+
+# Bootstrap salstack, we'll use it masterless :
 wget -O - https://bootstrap.saltstack.com  -O -|sh
+
+# Default salt minion config :
 cat > /etc/salt/minion <<EOF
 #all default
 EOF
+
+# If no salt repo in /srv yet, clone it
 [ ! -d /srv/salt ] && git clone https://github.com/ProjectUn1c0rn/SaltStrap.git /srv/salt
+
+# Making sure git repo is up to date
 cd /srv/salt
+git pull
 git submodule init
+git submodule update
+
+# Masterless mode, using /srv/salt 
 salt-call --local state.highstate
-shutdown -r 1 "Now rebooting to finalize tor isolation\!"
