1f00bb0a62df4671593c7078571026a1be8f1dcc
ProjectUn1c0rn <project.un1c0rn@yandex.com>
2014-07-07 09:37:14 +0200

    Update bootstrap.sh
    Adding a dev mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# if dev mode, just run salt
+[[ -n "${DEV_MODE}" ]] && salt-call --local state.highstate && exit 0
+
 # Debian testing needs this :
 export BS_PIP_ALLOWED=0
 
@@ -17,7 +21,7 @@ EOF
 # If no salt repo in /srv yet, clone it
 [ ! -d /srv/salt ] && git clone -b ${SALTSTRAP_GIT_BRANCH} ${SALTSTRAP_GIT_URL} /srv/salt
 
-# Making sure git repo is up to date
+# Making sure git repo is up to datez
 cd /srv/salt
 git pull
 git submodule init
