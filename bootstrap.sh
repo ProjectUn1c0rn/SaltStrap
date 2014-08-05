537c9f9ed619057c3dd505212ab8840e7e7bbf05
ProjectUn1c0rn <project.un1c0rn@yandex.com>
2014-08-05 15:20:43 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@
 #setting salt repo :
 wget -q -O- "http://debian.saltstack.com/debian-salt-team-joehealy.gpg.key" | apt-key add -
 echo "deb http://debian.saltstack.com/debian jessie-saltstack main" > /etc/apt/sources.list.d/saltstack.list
-apt-get update&&apt-get install git python-msgpack python-zmq salt-common -q -y
+apt-get update
+apt-get install python-msgpack -q -y #allow failling on debian stable
+apt-get install git python-zmq salt-common -q -y
 
 #clone the git repo :
 [ ! -d /srv/salt ] && git clone -b ${SALTSTRAP_GIT_BRANCH} ${SALTSTRAP_GIT_URL} /srv/salt
